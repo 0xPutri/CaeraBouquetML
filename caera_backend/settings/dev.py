de13192cb2f3adf53d46fd9b5ebbbd49ba668edf
0xPutri <124589883+0xPutri@users.noreply.github.com>
2026-03-20 09:48:29 +0700

    feat: initialize ML service base structure

---
+++ caera_backend/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
