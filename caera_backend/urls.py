d8ac92842c83325095cf73a94be46fc4a1f39d26
0xPutri <124589883+0xPutri@users.noreply.github.com>
2026-05-09 21:41:55 +0700

    feat: add branding icons for consistent visual identity

--- caera_backend/urls.py
+++ caera_backend/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from recommendation.views import MLHealthCheckView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/icons/favicon.ico', permanent=True)),
     path('health/', MLHealthCheckView.as_view(), name='ml_health_check'),
     path('api/recommendations/', include('recommendation.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
